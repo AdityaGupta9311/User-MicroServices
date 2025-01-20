6e87ac68ed8640c24410f430db606a0a5b29896c
Aditya Gupta <aditya.gupta93117@gmail.com>
2025-01-20 17:49:44 +0530

    DataBase Connect

--- src/main/java/com/user/Modals/Users.java
+++ src/main/java/com/user/Modals/Users.java
@@ -1,5 +1,7 @@
 package com.user.Modals;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,17 +15,100 @@ public class Users {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long Id;
-	
-	private String name;
-	
-	private String email;
-	
-	private String passowrd;
-	
-	private String city;
-	
-	private long phoneNo;
-	
 
-	
+	private String name;
+
+	private String email;
+
+	private String passowrd;
+
+	private String city;
+
+	private long phoneNo;
+
+	private LocalDateTime createdAt;
+
+	private LocalDateTime updatedAt;
+
+	public Users() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Users(long id, String name, String email, String passowrd, String city, long phoneNo,
+			LocalDateTime createdAt, LocalDateTime updatedAt) {
+		super();
+		Id = id;
+		this.name = name;
+		this.email = email;
+		this.passowrd = passowrd;
+		this.city = city;
+		this.phoneNo = phoneNo;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassowrd() {
+		return passowrd;
+	}
+
+	public void setPassowrd(String passowrd) {
+		this.passowrd = passowrd;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
 }
--- src/main/java/com/user/Services/UserService.java
+++ src/main/java/com/user/Services/UserService.java
@@ -1,5 +1,22 @@
 package com.user.Services;
 
-public class UserService {
+import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.user.Modals.Users;
+import com.user.Repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public Users createUser(Users users) {
+		users.setCreatedAt(LocalDateTime.now());
+		users.setUpdatedAt(LocalDateTime.now());
+		return userRepository.save(users);
+	}
 }
