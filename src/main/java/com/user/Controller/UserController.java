893df3ae239162ea139fe1f64fcf67a435b0dd2f
Aditya Gupta <aditya.gupta93117@gmail.com>
2025-01-25 22:01:23 +0530

    Sign In, Sign Up

--- src/main/java/com/user/Controller/UserController.java
+++ src/main/java/com/user/Controller/UserController.java
@@ -1,10 +1,12 @@
 package com.user.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.user.Modals.Users;
+import com.user.Repository.UserRepository;
 import com.user.Services.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,15 +18,28 @@ public class UserController {
 
 	@Autowired
 	UserService userService;
-
+	
+	@Autowired
+	UserRepository userRepository;
+	
 	@PostMapping("/create")
-	public ResponseEntity<?> registerUser(@RequestBody Users users) {
-		return userService.createUser(users);
+	public ResponseEntity<String> registerUser(@RequestBody Users users) {
+		Users existUseremail = userRepository.findByEmail(users.getEmail());
+		if (existUseremail != null) {
+			return new ResponseEntity<>("This Email is already exist",HttpStatus.CONFLICT);
+		}
+		
+		Users existUserphoneno = userRepository.findByPhoneNo(users.getPhoneNo());
+		if (existUserphoneno != null) {
+			return new ResponseEntity<>("This Phone Number is already exist",HttpStatus.CONFLICT);
+		}
+		
+		return new ResponseEntity<>("User Successfully Added",HttpStatus.CREATED);
 	}
 
 	@GetMapping("/login")
-	public Users login(@RequestBody Users users) throws Exception {
-		return userService.loginUser(users.getEmail(), users.getPassword());
+	public ResponseEntity<String> login(@RequestBody Users users) throws Exception {
+		return new ResponseEntity<>("Login Successfull",HttpStatus.ACCEPTED);
 	}
 
 }
--- src/main/java/com/user/Services/UserService.java
+++ src/main/java/com/user/Services/UserService.java
@@ -4,10 +4,7 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpServerErrorException;
 
 import com.user.Modals.Users;
 import com.user.Repository.UserRepository;
@@ -18,20 +15,10 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 
-	public ResponseEntity<?> createUser(Users users) {
-		Users existUseremail = userRepository.findByEmail(users.getEmail());
-		if (existUseremail != null) {
-			return new ResponseEntity<>("This Email is already exist",HttpStatus.CONFLICT);
-		}
-		
-		Users existUserphoneno = userRepository.findByPhoneNo(users.getPhoneNo());
-		if (existUserphoneno != null) {
-			return new ResponseEntity<>("This Phone Number is already exist",HttpStatus.CONFLICT);
-		}
-		
+	public Users createUser(Users users) {
 		users.setCreatedAt(LocalDateTime.now());
 		users.setUpdatedAt(LocalDateTime.now());
-		return new ResponseEntity<>("User Successfully Added",HttpStatus.CREATED);
+		return userRepository.save(users);
 	}
 
 	public Users loginUser(String email, String password) throws Exception {
